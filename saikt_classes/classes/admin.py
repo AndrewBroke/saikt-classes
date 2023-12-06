bf58d58711b9e9e31cd1aaaee22f9b62041d5802
Vasiliy003 <petapetkin82@gmail.com>
2023-12-06 15:45:06 +0700

    activating models

--- saikt_classes/classes/admin.py
+++ saikt_classes/classes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Weekdays)
+admin.site.register(Group)
+admin.site.register(Role)
+admin.site.register(Achievement)
+admin.site.register(Student)
+admin.site.register(LogEvent)
