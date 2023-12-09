db9481ee3be21d48ef3692970441fac2f3d9d845
Vasiliy003 <petapetkin82@gmail.com>
2023-12-09 17:53:08 +0700

    Заполнение бдшки (кроме достижений и ролей)

--- saikt_classes/classes/models.py
+++ saikt_classes/classes/models.py
@@ -3,23 +3,34 @@ from django.db import models
 # Create your models here.
 class Course(models.Model):
     name = models.CharField(max_length=150, null=False, blank=False)
+    def __str__(self):
+        return self.name
 
 class Weekdays(models.Model):
     name = models.CharField(max_length=150, null=False, blank=False)
+    def __str__(self):
+        return self.name
 
 class Group(models.Model):
     course = models.ForeignKey(
         'Course', on_delete=models.CASCADE
     )
     weekdays = models.ManyToManyField(Weekdays)
-    time = models.DateTimeField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return str(self.course) + " | " + str(self.weekdays)
 
 class Role(models.Model):
     name = models.CharField(max_length=150, null=False, blank=False)
+    def __str__(self):
+        return self.name
 
 class Achievement(models.Model):
     name = models.CharField(max_length=150, null=False, blank=False)
     image = models.ImageField(blank=True, upload_to='images/')
+    def __str__(self):
+        return self.name
 
 class Student(models.Model):
     course_id = models.ForeignKey('Group', on_delete=models.CASCADE)
@@ -27,11 +38,15 @@ class Student(models.Model):
     age = models.IntegerField(null=False)
     name = models.CharField(max_length=150, null=False, blank=False)
     surname = models.CharField(max_length=150, null=False, blank=False)
-    achievement = models.ManyToManyField(Achievement)
-    role = models.OneToOneField(Role, on_delete=models.CASCADE, null=True)
+    achievement = models.ManyToManyField(Achievement, null=True, blank=True)
+    role = models.OneToOneField(Role, on_delete=models.CASCADE, null=True, blank=True)
     is_right_hand = models.BooleanField(default=False)
+    def __str__(self):
+        return self.name + " || " + str(self.course_id)
 
 class LogEvent(models.Model):
     description = models.TextField(null=False, blank=True)
     datetime = models.DateTimeField()
     user = models.ManyToManyField(Student)
+    def __str__(self):
+        return self.datetime
