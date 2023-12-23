b4574f44697944b7f0d4cb256f739e2a3310eb39
Vasiliy003 <petapetkin82@gmail.com>
2023-12-23 18:02:18 +0700

    Что-то корявое но ладно

--- saikt_classes/classes/views.py
+++ saikt_classes/classes/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 # Create your views here.
 
 def index(request):
+    groups = Group.objects.all()
+
     context = {
-        "title": "Классы"
+        "title": "Классы",
+        "groups": groups
     }
     return render(request, 'classes/index.html', context)
 
